f3dee23fd6a4f3048d891b08ec3c59cc25248c5e
jeff1108 <jeffhongkong2007@gmail.com>
2018-07-23 16:27:45 +0100

    write nicer test without testing library - assert

--- note-model-test.js
+++ note-model-test.js
@@ -2,17 +2,18 @@
   function testNoteDefaultText() {
     var note = new Note();
 
-    if (note.text !== "My first note, woo") {
-      throw new Error("Your note isn't starting with the right default text");
-    }
+    assert.isTrue(note.text === "My first note, woo");
   }
   function testReturnText() {
     var note = new Note();
 
-    if (note.returnText() !== "My first note, woo") {
-      throw new Error("Your note is not work");
-      }
-    }
+    assert.isTrue(note.returnText() === "My first note, woo")
+  }
+    // if (note.returnText() !== "My first note, woo") {
+    //   throw new Error("Your note is not work");
+    //   }
+
+
 
   testNoteDefaultText();
   testReturnText();
