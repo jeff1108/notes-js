f040e928313b1918c079ed8fc1ee1f15b805ffe2
bibi collins <bibicollins94@googlemail.com>
2018-07-23 16:08:05 +0100

    Adding return text function and test

--- note-model-test.js
+++ note-model-test.js
@@ -5,8 +5,15 @@
     if (note.text !== "My first note, woo") {
       throw new Error("Your note isn't starting with the right default text");
     }
+  }
+  function testReturnText() {
+    var note = new Note();
 
-  };
+    if (note.returnText() !== "My first note, woo") {
+      throw new Error("Your note is not work");
+      }
+    }
 
   testNoteDefaultText();
+  testReturnText();
 })(this);
--- note-model.js
+++ note-model.js
@@ -1,6 +1,12 @@
-(function(exports){
- function Note(){
-  this.text = "My first note, woo"
-};
-exports.Note = Note;
-}) (this)
+(function(exports) {
+  function Note() {
+    this.text = 'My first note, woo'
+  };
+  Note.prototype.returnText = function() {
+    return this.text;
+  }
+  exports.Note = Note;
+})(this)
+
+var note
+note = console.log(Note());
